ff2f980ba0a1ceec45ef817ce664fdea715f4ba0
Sara McCombs <thesaramccombs@gmail.com>
2019-08-25 20:19:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,26 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+# method named key_for_min_value
+# method accepts a hash 
+# method iterates over the hash 
+# method returns the key that points to the smallest value of the set 
+# method should return nil if passed an empty hash
+
+
+
 def key_for_min_value(name_hash)
+  key = nil 
+  
+  if #hash is empty
+    return key
+  else
+    #store the first key in the hash to a return variable 
+    #iterate through the hash using collect 
+    #compare the value of the stored key to the next value in the hash
+    #retain whatever value is smallest 
+    
+    
+  end
 
 end
